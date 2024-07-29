0b64e39bc46052b4b6e55045681eb4127f1f3b0c
liuya <1522962872@qq.com>
2024-07-29 16:28:16 +0800

    feat: role

--- src/modules/role/role.service.ts
+++ src/modules/role/role.service.ts
@@ -1,22 +1,58 @@
 import { Injectable } from '@nestjs/common';
 import { CreateRoleDto } from './dto/role.dto';
+import { QueryRoleDto } from './dto/role.query.dto';
+import { PrismaClient } from '@prisma/client';
+import { BusinessException } from '../../common/exceptions/business.exceptions';
 
 @Injectable()
 export class RoleService {
-  create(createRoleDto: CreateRoleDto) {
-    console.log(createRoleDto);
-    return 'This action adds a new user';
+  constructor(private readonly prismaClient: PrismaClient) {}
+  async create(createRoleDto: CreateRoleDto) {
+    const { name } = createRoleDto;
+    const user = await this.prismaClient.role.findUnique({
+      where: {
+        name
+      }
+    });
+    user && BusinessException.throwCommonIncorrect('角色名称已存在');
+    await this.prismaClient.role.create({
+      data: createRoleDto
+    })
+    return null;
   }
 
-  findAll() {
-    return `This action returns all user`;
+  async findAll(query: QueryRoleDto) {
+    // 基于偏移分页
+    const { pageNum, pageSize } = query;
+    const skip = (pageNum - 1) * pageSize;
+    const take = pageSize;
+    const results = await this.prismaClient.role.findMany({
+      skip,
+      take,
+      orderBy: {
+        createdAt: 'asc'
+      }
+    });
+    return results;
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} user`;
+  async findOne(id: number) {
+    !id && BusinessException.throwCommonIncorrect('角色ID不能为空');
+    const user = await this.prismaClient.role.findUnique({
+      where: {
+        id
+      }
+    });
+    return user
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} user`;
+  async remove(id: number) {
+    !id && BusinessException.throwCommonIncorrect('角色ID不能为空');
+    await this.prismaClient.role.delete({
+      where: {
+        id
+      }
+    });
+    return null
   }
 }
