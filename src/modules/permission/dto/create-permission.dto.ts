b224a4820b985f03c71e10395a10473f474df918
liuya <1522962872@qq.com>
2024-07-29 10:30:09 +0800

    fix: 优化表设计

--- src/modules/permission/dto/create-permission.dto.ts
+++ src/modules/permission/dto/create-permission.dto.ts
@@ -1,6 +1,11 @@
 import { ApiProperty } from 'uni-nest';
+import { PermissionType } from '@prisma/client';
 
 export class CreatePermissionDto {
+  @ApiProperty({
+    description: '父节点'
+  })
+  parentId: number;
   @ApiProperty({
     description: '权限名',
     required: true
@@ -8,7 +13,38 @@ export class CreatePermissionDto {
   name: string;
 
   @ApiProperty({
-    description: '描述',
+    description: '排序'
+  })
+  order: number;
+
+  @ApiProperty({
+    description: '菜单图标'
+  })
+  icon: string;
+
+  @ApiProperty({
+    description: '组件路径'
+  })
+  component: string;
+
+  @ApiProperty({
+    description: '菜单图标'
+  })
+  path: string;
+
+  @ApiProperty({
+    description: '权限类型(MENU BUTTON)',
+    enum: PermissionType
+  })
+  type: number;
+
+  @ApiProperty({
+    description: '权限状态(1:启用 0:禁用)'
+  })
+  status: number;
+
+  @ApiProperty({
+    description: '描述'
   })
   des: string;
 }
