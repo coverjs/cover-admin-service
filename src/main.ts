57db77c231cdedc97c50e4e0aba3a0d27a731acd
hacxy <hacxy.js@outlook.com>
2024-11-08 09:54:59 +0800

    fix: 移除接口地址前缀

--- src/main.ts
+++ src/main.ts
@@ -6,10 +6,10 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors();
-  app.setGlobalPrefix('api');
+
   // load swagger docs
   loadSwagger(app);
 
-  await app.listen(process.env.PORT ?? 3000);
+  await app.listen(process.env.PORT ?? 1118);
 }
 bootstrap();
