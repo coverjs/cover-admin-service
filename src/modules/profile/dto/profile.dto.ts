514e9728b4b021a9da9991b5b59a8de4b4e68dcd
hacxy <hacxy.js@outlook.com>
2024-11-08 10:28:22 +0800

    fix: 个人信息接口规划

---
+++ src/modules/profile/dto/profile.dto.ts
@@ -0,0 +1,4 @@
+import { UserOptionalDto } from '@/modules/user/dto/user.dto';
+
+// 更新个人信息
+export class UpdateProfileDto extends UserOptionalDto {}
--- src/modules/profile/profile.controller.ts
+++ src/modules/profile/profile.controller.ts
@@ -1,10 +1,11 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, Patch } from '@nestjs/common';
 import { ProfileService } from './profile.service';
 import { User } from '@/common/decorators/user';
 import { ApiOperation, ApiTags } from '@nestjs/swagger';
 import { CommonApiResponse } from '@/common/decorators/apiResponse';
 import { ProfileVo } from './dto/profile.vo';
 import { UserInfoByParseToken } from '@/common/dto';
+import { UpdateProfileDto } from './dto/profile.dto';
 
 @Controller('profile')
 @ApiTags('个人信息')
@@ -16,5 +17,19 @@ export class ProfileController {
   @CommonApiResponse({ type: ProfileVo })
   async findUserInfo(@User() user: UserInfoByParseToken) {
     return this.profileService.getUserInfo(user);
+  }
+
+  @Patch()
+  @ApiOperation({ summary: '修改个人信息' })
+  @CommonApiResponse()
+  async updateUserInfo(@User() user: UserInfoByParseToken, @Body() body: UpdateProfileDto) {
+    console.log(user, body);
+  }
+
+  @Patch('password')
+  @ApiOperation({ summary: '重置密码' })
+  @CommonApiResponse()
+  async resetPassword(@User() user: UserInfoByParseToken) {
+    console.log(user);
   }
 }
